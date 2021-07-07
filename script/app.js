28419fe10225d748e57618bed33bc9c15c62695d
prathapmohan27 <prathapmohan2704@gmail.com>
2021-07-07 18:13:34 +0530

    etch a sketch first commit

---
+++ script/app.js
@@ -0,0 +1,67 @@
+function makeGrid(defaultValue){
+
+    const container=document.createElement('div');
+    container.id='main';
+    container.className='container';
+    document.body.appendChild(container);
+
+    const main=document.querySelector('#main');
+    
+    for(let i=0;i<defaultValue;i++){
+        const row=document.createElement('div');
+        row.id='row'+i;
+        row.className='row';
+        main.appendChild(row);
+        var rows = document.getElementById('row'+i);
+        for (var j=0; j<defaultValue; j++) {
+            var box = document.createElement("div");
+            box.className = "box";
+            rows.appendChild(box);
+        }
+    }
+    
+    main.setAttribute('style','grid-template-columns: repeat(' + defaultValue + ', 1fr)');
+    const gridRow=document.querySelector(".row");
+    gridRow.setAttribute('style','grid-template-rows: repeat(' + defaultValue + ', 1fr)');
+
+    gridBox=document.querySelectorAll(".box");
+    gridBox.forEach(div => {
+        div.addEventListener('mouseover', () => {
+            // const randomR = Math.floor(Math.random() * 256);
+            // const randomG = Math.floor(Math.random() * 256);
+            // const randomB = Math.floor(Math.random() * 256);
+            // div.style.background=`rgb(${randomR}, ${randomG}, ${randomB})`;
+            div.style.background='black';
+          });
+    });
+
+  
+}
+
+function changeSize(){ 
+    var newSize=prompt("Enter New Size 1 to 100");
+    newSize=parseInt(newSize);
+    if(Number.isNaN(newSize)){
+      return;
+    }
+   else if(100<newSize||newSize<1){
+        alert("Enter the valid number");
+       changeSize();
+    }
+    else{
+        clear()
+        makeGrid(newSize);
+    }  
+    
+}
+
+const changeButton=document.getElementById("change-size");
+changeButton.addEventListener('click',changeSize);
+
+function clear(){
+const gridContainer=document.getElementById('main');
+gridContainer.parentNode.removeChild(gridContainer);
+}
+
+
+window.onload = makeGrid(defaultValue=16);
